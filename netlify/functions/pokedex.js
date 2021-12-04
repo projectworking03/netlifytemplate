f18f671634f3332efb85b8e4087bd385164553d6
RisingDeadXTR <projectworking03@gmail.com>
2021-12-05 01:45:24 +0530

    feature:new

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,13 +1,18 @@
 const axios = require('axios')
 
-exports.handler = async function(){
-    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
+exports.handler = async function(event, context){
+    const eventBody = JSON.parse(event.body)
+
+    console.log(event)
+    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/'+ eventBody.region
 
     const response = await axios.get(POKE_API).then(res => res.data)
     
 
     return {
         statusCode: 200,
-        body: JSON.stringify(response)
+        body: JSON.stringify({
+            pokemin: response.pokemon_entries
+            })
     }
 }
