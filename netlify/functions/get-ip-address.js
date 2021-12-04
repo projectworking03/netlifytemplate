2d5bae79bddaa009d00466a855ec8ea59af37cb3
RisingDeadXTR <projectworking03@gmail.com>
2021-12-05 02:06:11 +0530

    feature:ip

--- netlify/functions/get-ip-address.js
+++ netlify/functions/get-ip-address.js
@@ -1,6 +1,9 @@
+const ip = require('ip')
+
 exports.handler = async function(event, context){
+    console.log(ip.address());
     return {
         statusCode: 200,
-        body: JSON.stringify(event)
+        body: JSON.stringify(ip.address)
     }
 }
