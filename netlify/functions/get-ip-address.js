aff69b4869362bd8815bae034b750bb1b09211c3
RisingDeadXTR <projectworking03@gmail.com>
2021-12-05 02:15:56 +0530

    gg

--- netlify/functions/get-ip-address.js
+++ netlify/functions/get-ip-address.js
@@ -5,6 +5,11 @@ exports.handler = async function(event, context){
     console.log(ipadd)
     return {
         statusCode: 200,
-        body: JSON.stringify(ipadd)
+        body: JSON.stringify({
+            add1: ip.address("private","ipv6"),
+            add2: ip.address("private","ipv4"),
+            add3: ip.address("public","ipv6"),
+            add4: ip.address("public","ipv4")
+        })
     }
 }
