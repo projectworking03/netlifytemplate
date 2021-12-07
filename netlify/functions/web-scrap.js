1ad0743f21ed861a17bc34c013205c4f3fd119d8
Aritra Paul <aritrapaul108@gmail.com>
2021-12-07 17:35:09 +0530

    modularised code

--- netlify/functions/web-scrap.js
+++ netlify/functions/web-scrap.js
@@ -1,153 +1,113 @@
-const axios = require("axios")
+const axios = require("axios");
 
+exports.handler = async function (event, context) {
+  // Form the URL
+  var movie_title = "Venom";
+  const url = `https://www.bing.com/search?q=${encodeURI(
+    movie_title + " movie"
+  )}`;
 
-exports.handler = async function(event, context){
-    var movie_title 
-    const url = "https://www.bing.com/search?q="+encodeURI(movie_title + " movie")
-    //const POKE_API = 'https://www.bing.com/search?q='+ eventBody.region
-    //var url = "https://api.allorigins.win/get?url=https://www.google.com/search?q=Venom:%20Let%20There%20Be%20Carnage"
-    //const url = "https://www.bing.com/search?q=venom"
-    //var file = fs.createWriteStream('./web.html');
-    const a = await axios({
-        method:'get',
-        url:url,
-        headers:{
-            Accept: 'application/json, text/plain, */*',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
-        }
-    })
-    resp = a.data
-    // await request({
-    //                 method:'get',
-    //                 url:url,
-    //                 headers:{
-    //                     Accept: 'application/json, text/plain, */*',
-    //                     'User-Agent': 'Chrome/79.0.3945.99'
-    //                 }
-    //             },function(error, response, body){
-    //                 resp = body
-    //                 //console.log(body)
-    //             }).pipe(file)
-    
-    try
-    {   
-        var str = JSON.stringify(resp)
-        const rating_reg = /<div class=\\"l_ecrd_ratings_prim\\">(\w.+?)</g
-        var ratings = [];
-        while (true) {
-            const match = rating_reg.exec(str);
-            if (match === null) break;
-            // Add capture of group 1 to `matches`
-            //console.log("MATCH:",match)
-            ratings.push(match[1]);
-        } 
-        console.log(ratings)
-        const prov_reg = /<div class=\\"l_ecrd_txt_qfttl\\">(\w.+?)</g
-        var rating_providers = [];
-        while (true) {
-            const match = prov_reg.exec(str);
-            if (match === null) break;
-            // Add capture of group 1 to `matches`
-            //console.log("MATCH:",match)
-            rating_providers.push(match[1]);
-        } 
-        console.log(rating_providers)
-        const watch_provider_reg = /<a class=\\"b_action b_accentColor actWebAction\\" (\w.+?)>(\w.+?)</g
-        const watch_url_reg = /href=\\"(\w.+?)\\"/g        
-        const pov_name_reg = />(\w.+?)</
-        var watch_list = [];
-        while (true) {
-            const match = watch_provider_reg.exec(str);
-            if (match === null) break;
-            // Add capture of group 1 to `matches`
-            //console.log("MATCH:",match)
-            else
-            {
-                const match_url = watch_url_reg.exec(match);
-                if(match_url === null)  break;
-                console.log(match_url.input)
-                const pov_name = pov_name_reg.exec(match)
-                watch_list.push({
-                    provider: pov_name[1],
-                    url:match_url[1]
-                });
-            }
-        } 
-        console.log(watch_list)
-        
-        const rating_with_prov = []
+  // Fetch the HTML
+  const html = await fetchHtml(url);
 
-        for(var i = 0; i < ratings.length; i++)
-        {
-            rating_with_prov.push({
-                provider:rating_providers[i],
-                rating:ratings[i]
-            })
-        }
+  // Extract the ratings and streaming platforms
+  const ratings = getRatings(html);
+  const streamingPlatforms = getStreamingPlatforms(html);
 
-        resp = {
-            ratings : rating_with_prov,
-            watch_list: watch_list
-        }
-    }
-    catch(err)
-    {
-        resp = 'Error'
-        console.log("ERR:",err)
-    }
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            data:resp})
-    }
-}
+  // Build the response
+  const response = {
+    ratings,
+    streamingPlatforms,
+  };
 
+  // Send the response
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response,
+    }),
+  };
+};
 
+// Fetches th HTML given a URL
+const fetchHtml = async (url) => {
+  const html = await axios({
+    method: "get",
+    url: url,
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
+    },
+  }).then((res) => JSON.stringify(res.data));
 
+  return html;
+};
 
+const getRatings = (html) => {
+  const ratingValues = getRatingValues(html);
+  const ratingProviders = getRatingProviders(html);
 
+  let ratings = [];
+  for (let i = 0; i < ratingValues.length; i++) {
+    const ratingProvider = ratingProviders[i];
+    const ratingValue = ratingValues[i];
+    ratings.push({ provider: ratingProvider, value: ratingValue });
+  }
 
+  return ratings;
+};
 
+// Get the rating values
+const getRatingValues = (html) => {
+  const ratingRegex = /<div class=\\"l_ecrd_ratings_prim\\">(\w.+?)</g;
 
+  let ratingValues = [];
+  while (true) {
+    // Extract all rating values matching the above regular expression
+    const match = ratingRegex.exec(html);
+    if (match === null) break;
+    ratingValues.push(match[1]);
+  }
 
+  return ratingValues;
+};
 
+// Get the rating providers
+const getRatingProviders = (html) => {
+  const providerRegex = /<div class=\\"l_ecrd_txt_qfttl\\">(\w.+?)</g;
 
+  let ratingProviders = [];
+  while (true) {
+    const match = providerRegex.exec(html);
+    if (match === null) break;
+    ratingProviders.push(match[1]);
+  }
 
+  return ratingProviders;
+};
 
+// Get the streaming platforms
+const getStreamingPlatforms = (html) => {
+  const watchProviderRegex =
+    /<a class=\\"b_action b_accentColor actWebAction\\" (\w.+?)>(\w.+?)</g;
+  const watchUrlRegex = /href=\\"(\w.+?)\\"/g;
+  const providerNameRegex = />(\w.+?)</;
 
+  let streamingPlatforms = [];
+  while (true) {
+    const match = watchProviderRegex.exec(html);
+    if (match === null) break;
 
+    const matchUrl = watchUrlRegex.exec(match);
+    if (matchUrl === null) break;
 
+    const providerName = providerNameRegex.exec(match);
+    streamingPlatforms.push({
+      provider: providerName[1],
+      url: matchUrl[1],
+    });
+  }
 
-
-
-
-
-
-// const puppeteer = require("puppeteer")
-
-// exports.handler = async function(event, context){
-//     var url = "https://api.allorigins.win/get?url=https://www.google.com/search?q=Venom:%20Let%20There%20Be%20Carnage"
-//     url = "https://www.bing.com/search?q=venom"
-    
-
-
-
-
-    // const browser = await puppeteer.launch()
-    // const page = await browser.newPage();
-    // await page.goto(url)
-
-    // const names = await page.evaluate(()=>{
-    //    return Array.from(document.querySelectorAll(".l_ecrd_ratings_prim")).map(x => x.textContent) 
-    // })
-    // const ratings = await page.evaluate(()=>{
-    //     return Array.from(document.querySelectorAll(".l_ecrd_txt_qfttl")).map(x => x.textContent) 
-    //  })
-    // browser.close()
-//     return {
-//         statusCode: 200,
-//         body: JSON.stringify({
-//             names:names,
-//             rating:ratings})
-//     }
-// }
+  return streamingPlatforms;
+};
